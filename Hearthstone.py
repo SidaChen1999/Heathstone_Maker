22d502d3734d032a2a7514154ba5e7e11b77524c
SidaChen1999 <schenci@connect.ust.hk>
2021-12-25 20:19:15 +0800

    optimize logging and statistics, update out_game and  main function

--- Hearthstone.py
+++ Hearthstone.py
@@ -7,44 +7,11 @@ from datetime import datetime
 import csv
 import logging
 import traceback
-FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(filename='std.log', format=FORMAT, filemode='w')
-logger = logging.getLogger('Hearthstone')
-logger.setLevel(logging.DEBUG) 
-
-print("script starts")
-logger.info("script starts")
-
-var = {'win': 0, 'loss': 0, 'error': 0, 'timestamp': datetime.now()}
-# win = 0
-# loss = 0
-# errors = 0
-# states = 0 # 0 = out of game; 1 = my turn; 2 = enemy turn; 3 = error
-# last_states = 0
-region = 'NA'
-img_start = 'G:\Gaming Script\start.jpg'
-img_loss = 'G:\Gaming Script\loss.jpg'
-img_win = 'G:\Gaming Script\win.jpg'
-img_confirm = 'G:\Gaming Script\confirm.jpg'
-img_end_turn = 'G:\Gaming Script\end_turn.jpg'
-img_enemy_turn = 'G:\Gaming Script\enemy_turn.jpg'
-img_my_turn = 'G:\Gaming Script\my_turn.jpg'
-img_my_turn1 = 'G:\Gaming Script\my_turn1.jpg'
-img_traditional_game = 'G:\Gaming Script\\traditional_game.jpg'
-img_play = 'G:\Gaming Script\play.jpg'
-img_battlenet = 'G:\Gaming Script\\battlenet.jpg'
-img_click = 'G:\Gaming Script\click.jpg'
-# regions = (left, top, width, height)
-cards = (960, 1200, 600, 50)
-minions = (700, 700, 1050, 30)
-hero = (1090, 970, 460, 30)
-green = (213, 255, 139)
-yellow = (255, 255, 12)
-green2 = (208, 233, 97)
-red = (255, 255, 89)
-enemy_hero = (1298, 415)
-confi = 0.8
-timeout = 120 # seconds
+import ctypes
+import win32con
+import win32gui
+import win32process
+import os
 
 def tuple_add(a:tuple, b:tuple):
     return tuple(map(lambda i, j: i + j, a, b))
@@ -54,7 +21,6 @@ def tuple_abs_sum(a:tuple):
     return sum([abs(number) for number in a])
 def delta(a, b):
     return tuple_abs_sum(tuple_sub(a, b))
-epsilon = 20
 
 def check_state(var, last_state, simple=False):
     cor_enemy_turn = pg.locateOnScreen(img_enemy_turn, grayscale=False, confidence=confi)
@@ -72,6 +38,7 @@ def check_state(var, last_state, simple=False):
         return next_state
     if last_state == next_state:
         if (datetime.now() - var['timestamp']).seconds > timeout:
+            var['timestamp'] = datetime.now()
             error_state(var)
             var['timestamp'] = datetime.now()
     else:
@@ -80,71 +47,46 @@ def check_state(var, last_state, simple=False):
 
 def error_state(var):
     var['error'] += 1
-    print('error: %i' % var['error'])
     logger.error('error: %i', var['error'])
-    flag = False
     cor_play = pg.locateOnScreen(img_play, grayscale=True, confidence=confi)
     if cor_play != None:
         x, y = pg.center(cor_play)
         pg.click(x=x,y=y, duration=0.5)
         time.sleep(10)
-        flag = True
-    elif checkIfProcessRunning('hearthstone'):
-        killProcess('hearthstone')
-        while pg.locateOnScreen(img_play, grayscale=True, confidence=confi) == None:
-            time.sleep(0.5)
-            if (datetime.now() - var['timestamp']).seconds > timeout:
-                break
-        if (datetime.now() - var['timestamp']).seconds > timeout:
+    else:
+        proc = checkIfProcessRunning('hearthstone', kill=True)
+        time.sleep(2)
+        if proc == None:
             cor_battlenet = pg.locateOnScreen(img_battlenet, grayscale=True, confidence=confi)
             if cor_battlenet != None:
                 x, y = pg.center(cor_battlenet)
                 pg.click(x=x,y=y, duration=0.5)
-                time.sleep(1)
+                time.sleep(2)
             else:
                 return
-        x, y = pg.center(pg.locateOnScreen(img_play, grayscale=True, confidence=confi))
-        pg.click(x=x,y=y, duration=0.5)
-        time.sleep(10)
-        flag = True
-    elif not checkIfProcessRunning('hearthstone'):
-        cor_battlenet = pg.locateOnScreen(img_battlenet, grayscale=True, confidence=confi)
-        if cor_battlenet != None:
-            x, y = pg.center(cor_battlenet)
-            pg.click(x=x,y=y, duration=0.5)
-            time.sleep(1)
+        else:
+            cor_play = pg.locateOnScreen(img_play, grayscale=True, confidence=confi)
+            if cor_play == None:
+                cor_battlenet = pg.locateOnScreen(img_battlenet, grayscale=True, confidence=confi)
+                if cor_battlenet != None:
+                    x, y = pg.center(cor_battlenet)
+                    pg.click(x=x,y=y, duration=0.5)
+                    time.sleep(2)
+                else:
+                    return
         cor_play = pg.locateOnScreen(img_play, grayscale=True, confidence=confi)
         if cor_play != None:
             x, y = pg.center(cor_play)
+            var['timestamp'] = datetime.now()
             pg.click(x=x,y=y, duration=0.5)
             time.sleep(10)
-        flag = True
-    if flag:
-        if region == 'CN':
-            # CN
-            while pg.locateOnScreen(img_click, grayscale=False, confidence=confi) == None:
-                time.sleep(2)
-                if (datetime.now() - var['timestamp']).seconds > timeout:
-                        return
-            pg.click(x=1280, y=1000, clicks=2, interval=1, duration=0.5)
-            while pg.locateOnScreen(img_traditional_game, grayscale=True, confidence=confi) == None:
-                time.sleep(2)
-                check_state(var, state, simple=True)
-                if state != 0:
-                    # var['timestamp'] = datetime.now()
-                    break
-                pg.click(x=1280, y=1000)
-                if (datetime.now() - var['timestamp']).seconds > timeout:
-                        return
-        else:
-            # NA
-            while pg.locateOnScreen(img_traditional_game, grayscale=True, confidence=confi) == None:
-                time.sleep(2)
-                pg.click(x=1280, y=1000)
-                if check_state(var, state, simple=True) != 0:
-                    break
-                if (datetime.now() - var['timestamp']).seconds > timeout:
-                    return
+    while pg.locateOnScreen(img_traditional_game, grayscale=True, confidence=confi) == None:
+        time.sleep(2)
+        pg.click(x=1280, y=1030)
+        if check_state(var, state, simple=True) != 0:
+            break
+        if (datetime.now() - var['timestamp']).seconds > timeout:
+            return
 
 last_minion = 0
 last_card = 0
@@ -170,7 +112,6 @@ def my_turn(last_minion, last_card):
                 break
         if flag == 1:
             break
-
     
     width, height = pic_minions.size
     flag = 0
@@ -236,85 +177,113 @@ def out_game(var):
 
     elif pg.locateOnScreen(img_loss, grayscale=True, confidence=confi) != None:
         var['loss'] += 1
-        print('win: %i; loss: %i' % (var['win'], var['loss']))
         logger.info('win: %i; loss: %i', var['win'], var['loss'])
         while pg.locateOnScreen(img_start, grayscale=False, confidence=confi) == None:
-            pg.click(x=1280, y=1050)
+            pg.click(x=1280, y=1030)
             time.sleep(1)
             if (datetime.now() - var['timestamp']).seconds > timeout:
                     return
     elif pg.locateOnScreen(img_win, grayscale=True, confidence=confi) != None:
         var['win'] += 1
-        print('win: %i; loss: %i' % (var['win'], var['loss']))
         logger.info('win: %i; loss: %i', var['win'], var['loss'])
         while pg.locateOnScreen(img_start, grayscale=False, confidence=confi) == None:
-            pg.click(x=1280, y=1050)
+            pg.click(x=1280, y=1030)
             time.sleep(1)
             if (datetime.now() - var['timestamp']).seconds > timeout:
                     return
 
-def checkIfProcessRunning(processName):
+def checkIfProcessRunning(processName, kill=False):
+    '''Return the process id if it is running or return None'''
     for proc in psutil.process_iter():
         try:
             if processName.lower() in proc.name().lower():
-                return True
+                if kill:
+                    proc.kill()
+                return proc.pid
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    return False
+    return None
 
-def killProcess(processName):
-    for proc in psutil.process_iter():
+if __name__ == '__main__':
+    user32 = ctypes.windll.user32
+    screensize = user32.GetSystemMetrics(0),  user32.GetSystemMetrics(1)
+    FORMAT = '%(asctime)s %(message)s'
+    log_file_start = 'log/'+datetime.now().strftime("%Y-%m-%d,%H;%M;%S")+'.log'
+    logging.basicConfig(filename=log_file_start, format=FORMAT, filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG) 
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logging.Formatter(FORMAT))
+    logger.addHandler(consoleHandler)
+    var = {'win': 0, 'loss': 0, 'error': 0, 'timestamp': datetime.now()}
+
+    img_start = 'G:\Gaming Script\start.jpg'
+    img_loss = 'G:\Gaming Script\loss.jpg'
+    img_win = 'G:\Gaming Script\win.jpg'
+    img_confirm = 'G:\Gaming Script\confirm.jpg'
+    img_end_turn = 'G:\Gaming Script\end_turn.jpg'
+    img_enemy_turn = 'G:\Gaming Script\enemy_turn.jpg'
+    img_my_turn = 'G:\Gaming Script\my_turn.jpg'
+    img_my_turn1 = 'G:\Gaming Script\my_turn1.jpg'
+    img_traditional_game = 'G:\Gaming Script\\traditional_game.jpg'
+    img_play = 'G:\Gaming Script\play.jpg'
+    img_battlenet = 'G:\Gaming Script\\battlenet.jpg'
+    img_click = 'G:\Gaming Script\click.jpg'
+    # regions = (left, top, width, height)
+    cards = (960, 1200, 600, 50)
+    minions = (700, 700, 1050, 30)
+    hero = (1090, 970, 460, 30)
+    green = (213, 255, 139)
+    yellow = (255, 255, 12)
+    green2 = (208, 233, 97)
+    red = (255, 255, 89)
+    enemy_hero = (1298, 415)
+    confi = 0.8
+    timeout = 120 # seconds
+    epsilon = 20
+    state = 1
+    logger.info("script starts")
+    while keyboard.is_pressed('q') == False:
         try:
-            if processName.lower() in proc.name().lower():
-                proc.kill()
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-
-state = 1
-while keyboard.is_pressed('q') == False:
-    try:
-        state = check_state(var, state)
-        print('state: %i' % state)
-        logger.info('state: %i', state)
-
-        # out of game
-        if state == 0:
-            out_game(var)
-
-        # my turn
-        elif state == 1:
-            my_turn(last_minion, last_card)
-
-        # enemy turn
-        elif state == 2:
-            time.sleep(1)
-
-    except (KeyboardInterrupt, pg.FailSafeException):
-        break
-    except:
-        logger.info(traceback.format_exc())
-        try:
-            error_state(var)
-            timestamp = datetime.now()
+            state = check_state(var, state)
+            logger.info('state: %i', state)
+            if state == 0:
+                out_game(var)
+            elif state == 1:
+                my_turn(last_minion, last_card)
+            elif state == 2:
+                time.sleep(1)
+        except (KeyboardInterrupt, pg.FailSafeException):
+            break
         except:
             logger.info(traceback.format_exc())
-            break
+            try:
+                error_state(var)
+                timestamp = datetime.now()
+            except:
+                logger.info(traceback.format_exc())
+                break
 
-
-print('win: %i; loss: %i; error: %i' % (var['win'], var['loss'], var['error']))
-logger.info('win: %i; loss: %i; error: %i', var['win'], var['loss'], var['error'])
-print("script ends")
-logger.info("script ends")
-
-if var['loss'] != 0:
+    logger.info('win: %i; loss: %i; error: %i; win rate: %.4f',
+        var['win'], var['loss'], var['error'], var['wins']/(var['win']+var['loss']))
+    logger.info("script ends")
     rows = []
-    with open('stats.csv', 'r', newline='') as csvfile:
+    with open('G:\Gaming Script\dist\stats.csv', 'r', newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         header = next(csvreader)
         for row in csvreader:
             rows.append(row)
-    with open('stats.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(header)
-        csvwriter.writerows(rows)
-        csvwriter.writerow([str(var[a]) for a in var])
+    if var['loss'] != 0:
+        rows.append([str(var[a]) for a in var])
+        with open('G:\Gaming Script\dist\stats.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(header)
+            csvwriter.writerows(rows)
+    wins, losses = 0, 0
+    for row in rows:
+        wins += int(row[0])
+        losses += int(row[1])
+    logger.info('total wins: %i, losses: %i, win rate: %.4f' %(wins, losses, wins/(wins+losses)))
+    logging.shutdown()
+    log_file_end = 'log/'+datetime.now().strftime("%Y-%m-%d,%H;%M;%S")+'.log'
+    os.rename(log_file_start, log_file_end)
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pyautogui as pg
 import time
-import keyboard
 import psutil
 from datetime import datetime
 img_start = 'G:\Gaming Script\start.jpg'
@@ -111,6 +110,96 @@ def my_turn():
 
 # pg.press('space', presses=1000, interval=0.5)
 
-var = {'win': 0, 'loss': 0, 'error': 0, 'timestamp': datetime.now()}
-var['error'] += 1
-print(var)
+import win32con
+import win32gui
+import win32process
+
+def isRealWindow(hWnd) -> bool:
+    '''Return True iff given window is a real Windows application window.'''
+    if not win32gui.IsWindowVisible(hWnd):
+        return False
+    if win32gui.GetParent(hWnd) != 0:
+        return False
+    hasNoOwner = win32gui.GetWindow(hWnd, win32con.GW_OWNER) == 0
+    lExStyle = win32gui.GetWindowLong(hWnd, win32con.GWL_EXSTYLE)
+    if (((lExStyle & win32con.WS_EX_TOOLWINDOW) == 0 and hasNoOwner)
+      or ((lExStyle & win32con.WS_EX_APPWINDOW != 0) and not hasNoOwner)):
+        if win32gui.GetWindowText(hWnd):
+            return True
+    return False
+
+def getWindowSizes() -> list:
+    '''
+    Return a list of tuples (handler, rect) for each real window.
+    Rect is a tuple of (left, top, right, button)
+    '''
+    def callback(hWnd, windows):
+        if not isRealWindow(hWnd):
+            return
+        rect = win32gui.GetWindowRect(hWnd)
+        windows.append((hWnd, rect))
+    windows = []
+    win32gui.EnumWindows(callback, windows)
+    return windows
+
+def get_hwnds_for_pid (pid):
+    def callback (hwnd, hwnds):
+        if win32gui.IsWindowVisible (hwnd) and win32gui.IsWindowEnabled (hwnd):
+            _, found_pid = win32process.GetWindowThreadProcessId (hwnd)
+            if found_pid == pid:
+                hwnds.append (hwnd)
+        return True
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds
+
+def checkIfProcessRunning(processName, kill=False):
+    '''Return the process id if it is running or return None'''
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+                if kill:
+                    proc.kill()
+                return proc.pid
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return None
+
+# game_window = (0, 0)
+# id = checkIfProcessRunning("lingoes")
+# if id is not None:
+#     hwnds = get_hwnds_for_pid(id)
+#     print(hwnds)
+# wins = getWindowSizes()
+# if len(hwnds) != 0:
+#     for win in wins:
+#         if hwnds[0] == win[0]:
+#             game_window = win[1]
+#             print(win[1])
+#             break
+
+import logging
+import os
+import logging
+FORMAT = '%(asctime)s %(message)s'
+log_file = 'log/'+datetime.now().strftime("%Y-%m-%d,%H;%M;%S")+'.log'
+logging.basicConfig(filename=log_file, format=FORMAT, filemode='w')
+
+# logFormatter = logging.Formatter(FORMAT)
+logger = logging.getLogger()
+
+# fileHandler = logging.FileHandler(log_file)
+# fileHandler.setFormatter(logFormatter)
+# logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logging.Formatter(FORMAT))
+logger.addHandler(consoleHandler)
+
+print(log_file)
+logger.setLevel(logging.DEBUG) 
+logger.info("script ends")
+time.sleep(10)
+logging.shutdown()
+new = 'log/'+datetime.now().strftime("%Y-%m-%d,%H;%M;%S")+'.log'
+os.rename(log_file, new)
